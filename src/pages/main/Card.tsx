4d73b80cb2130ff341ae327f7efda3cca70219e6
Dexter Fort <dexterkun16@gmail.com>
2023-05-28 00:08:06 +0800

    admin refactored

--- src/pages/main/Card.tsx
+++ src/pages/main/Card.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Card = () => {
-  return <div className='main-panel no-card-section'>Card</div>;
+  return (
+    <div className='main-panel no-card-section'>
+      <h1>Card</h1>
+      <div className='container'></div>
+    </div>
+  );
 };
 
 export default Card;
